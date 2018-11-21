d63833d29e45adb4f7bd2bce85c39be9cbb2eaab
GAWSSODIA\LuisMiguel <luispalacio1997@hotmail.com>
2018-11-20 23:08:01 -0500

    initial commmit

---
+++ index.js
@@ -0,0 +1,16 @@
+// const button = document.querySelectorAll(".myButton");
+
+// button.addEventListener("click", () => {
+//     var expandImg = document.getElementById("expandedImg");
+//     expandImg.src = button.src;
+//     expandImg.parentElement.style.display = "block";
+// })
+
+// (function () {
+
+//     function showImg(imgs){
+//         var expandImg = document.getElementById("expandedImg");
+//         expandImg.src = imgs.src;
+//         expandImg.parentElement.style.display = "block";
+//     }
+// })();
