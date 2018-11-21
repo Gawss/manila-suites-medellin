aa8cbd72e6d3ae95e351b0c3134c70ddfb4ad120
GAWSSODIA\LuisMiguel <luispalacio1997@hotmail.com>
2018-11-20 23:33:25 -0500

    server

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+let SERVER_PORT = 1337;
+
+app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/Source'));
+
+const server = app.listen(process.env.PORT || SERVER_PORT, () => {
+console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env)
+console.log('Server On', __dirname)
+});
+
+
+
+app.get('/', (req, res) => {
+
+    console.log('GET /');
+    res.sendFile(__dirname + '/index.html');
+});
