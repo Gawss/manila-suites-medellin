85f6fefa48219ca06b25ad55e6826f2dd2069f5e
GAWSSODIA\LuisMiguel <luispalacio1997@hotmail.com>
2018-11-24 00:24:08 -0500

    studiofinca@gmail.com

--- Config/config.js
+++ Config/config.js
@@ -1,4 +1,5 @@
-const REALTOR_EMAIL = 'support@stellarviewrealty.com';
+// const REALTOR_EMAIL = 'support@stellarviewrealty.com';
+const REALTOR_EMAIL = 'studiofinca@gmail.com';
 
 module.exports = {
     REALTOR_EMAIL
