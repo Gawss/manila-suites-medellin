5cc294d5ce9ac995f90a5db2588cc3094b9556b2
GAWSSODIA\LuisMiguel <luispalacio1997@hotmail.com>
2018-11-23 23:45:42 -0500

    sendEmail

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const sendEmail = require('./GmailAPI/sendEmail');
 const app = express();
 
 let SERVER_PORT = 1337;
 
 app.use(express.static(__dirname + '/'));
 app.use(express.static(__dirname + '/Source'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const server = app.listen(process.env.PORT || SERVER_PORT, () => {
 console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env)
@@ -19,6 +23,17 @@ app.get('/', (req, res) => {
 
 app.post('/notification', (req, res) => {
 
-    console.log(req);
+    console.log(req.body);
+    let name = req.body.NAME;
+    let email = req.body.EMAIL;
+    let phone = req.body.PHONE;
+
+    let message =   'Looks like an investor is interested!' + 
+                    '\nHere is the contact information: '   +
+                    '\n\nName: ' + name                    +
+                    '\nE-mail: ' + email                    +
+                    '\nPhone: ' + phone                     +
+                    '\n\n Good Luck!';
+    sendEmail(email, message);
     res.send('Ok');
 });
